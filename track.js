f0cebf3292d3442ddaef0285ee4061abfe31d4e0
hamik112 <hamik112@gmail.com>
2014-12-22 21:23:16 -0800

    Update track.js

--- track.js
+++ track.js
@@ -1,3 +1,47 @@
+var landingpage_id = '1';
+var aff_id = getValue('aff_id');
+var c1 = var_aff_id;
+var c2 = getValue('c2');
+var c3 = getValue('c3');
+var c4 = getValue('c4');
+var c5 = getValue('c5');
+var t1 = getValue('t1');
+var uniqid = check_cookie('uniqid');
+
+function track(url)
+    {
+           
+        var feedApiGetJSON = url+"/"+c1+"/"+c2+"/"+c3+"/"+c4+"/"+c5+"/"+t1+"/"+uniqid;
+            $.ajax({
+                url: feedApiGetJSON + feedUrl,
+                dataType: 'jsonp',
+                jsonpCallback: 'JsonpCallback'
+            });
+    }
+
+    function JsonpCallback(data){
+        if (data.responseStatus == "200")
+        var track_id = response.responseData.session_cookie;
+        check_cookie('uniqid',track_id);
+           setTimeout(function(){
+                  
+                   var = check_cookie('uniqid',track_id);
+                       var feedApiGetJSON = url+"/"+c1+"/"+c2+"/"+c3+"/"+c4+"/"+c5+"/"+t1+"/"+uniqid;
+            $.ajax({
+                url: feedApiGetJSON + feedUrl,
+                dataType: 'jsonp',
+                jsonpCallback: 'JsonpCallback'
+            });
+       
+    },15000);}
+   
+            
+
+                   
+  
+            
+
+
 function getValue(variable)
 {
        var query = window.location.search.substring(1);
@@ -19,25 +63,27 @@ function create_hidden_input(form_id,name,value)
 }
 
 
-function check_cookie(name)
+function check_cookie(name,value)
 {
        if($.cookie(name)==undefined)
        {
               rand_num = Math.floor(Math.random() * (max - min)) + min;
               $.cookie(name,rand_num, { expires: 7, path: '/' });
+              return "";
        }
-       return $.cookie(name);
+       else if($.cookie(name)!=value)
+       {   
+              $.cookie(name,value, { expires: 7, path: '/' });
+              return "";
+              
+       }
+       else{
+              return $.cookie(name);
+
+       }
 }
 
-var landingpage_id = '1';
-var aff_id = getValue('aff_id');
-var c1 = var_aff_id;
-var c2 = getValue('c2');
-var c3 = getValue('c3');
-var c4 = getValue('c4');
-var c5 = getValue('c5');
-var t1 = getValue('t1');
-var uniqid = check_cookie('uniqid');
+
 
 //get_token = get_token()
 
