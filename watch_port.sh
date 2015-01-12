be2a5f1830dc5ef2606cac5ce2be6a9668d3849f
rivir99 <rivir99@gmail.com>
2015-01-12 15:42:02 -0500

    watch dog bug smashin

--- watch_port.sh
+++ watch_port.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-netstat -lp | grep 55555
-if [[ $? != 0 ]]; then screen -d -m -L -S autorestart "cd /root/crm_trm/ && python orderapi.py"; fi
+netstat -p | grep 55555
+if [[ $? != 0 ]]; then screen -d -m -L -S autorestart "cd /root/crm_trm/ && python orderapi.py"; echo "restarted server"; else echo "server is live"; fi
